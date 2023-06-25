1229938af369597a7958657326926a2032a96831
Nirob Hasan <nirob427417@gmail.com>
2023-06-25 21:20:13 +0600

    user authorization

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { UsersController } from './user.controller';
 
 const router = express.Router();
 
-router.get('/:id', UsersController.getSingleUser);
-router.patch('/:id', UsersController.updateUser);
-router.delete('/:id', UsersController.deleteUser);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UsersController.getSingleUser);
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), UsersController.updateUser);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UsersController.deleteUser);
 // router.post('/signup', UsersController.createUser);
-router.get('/', UsersController.getAllUsers);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UsersController.getAllUsers);
 
 export const UserRoutes = router;
