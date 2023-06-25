f63940b56abd1529b677119fb60a6d0a2755ed6c
Nirob Hasan <nirob427417@gmail.com>
2023-06-25 21:54:56 +0600

    cow authorization

--- src/app/modules/cow/cow.route.ts
+++ src/app/modules/cow/cow.route.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { CowController } from './cow.controller';
 
 const router = express.Router();
 
-router.get('/:id', CowController.getSingleCow);
-router.patch('/:id', CowController.updateCow);
-router.delete('/:id', CowController.deleteCow);
-router.post('/', CowController.createCow);
-router.get('/', CowController.getAllCows);
+router.get(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.BUYER),
+  CowController.getSingleCow
+);
+router.patch('/:id', auth(ENUM_USER_ROLE.SELLER), CowController.updateCow);
+router.delete('/:id', auth(ENUM_USER_ROLE.SELLER), CowController.deleteCow);
+router.post('/', auth(ENUM_USER_ROLE.SELLER), CowController.createCow);
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.BUYER),
+  CowController.getAllCows
+);
 
 export const CowRoutes = router;
